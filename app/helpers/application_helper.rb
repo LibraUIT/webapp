d589bc1e5f520f958ec9af0ab08af86a1059981c
Libra <quanmn.libra@gmail.com>
2017-02-05 00:03:55 +0700

    Added landing page :smile:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def body_data
+    data = { view: "#{controller_path}##{action_name}" }
+    data[:debug] = 'true' if Rails.env.development?
+    data
+  end
+
+  def body_css_class
+    controller_name = controller_path.underscore.tr('/', '-')
+    "#{controller_name} #{controller_name}-#{css_action_name}"
+  end
+
+  def css_action_name
+    action = action_name
+    case action
+    when 'update'
+      'edit'
+    when 'create'
+      'new'
+    else
+      action
+    end.tr('_', '-')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'app/dashboard#index'
+
+  namespace :admin do
+    get '/', to: redirect('/admin/dashboard')
+    resources :dashboard, only: [:index]
+  end
+
+  namespace :app do
+    get '/', to: redirect('/app/dashboard')
+    resources :dashboard, only: [:index]
+  end
+
 end
